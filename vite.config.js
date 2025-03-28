014095b2eb7e2aef8ab69ca1a30a4a6c5074a905
muhanaskt <anas.kt@webandcrafts.in>
2025-03-28 11:00:36 +0530

    not found

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  alias: {
+    '@': '/src', // Ensure this points to your src directory
+  },
 })
